7afb5fcb27aa70e0852f0bb7a4dd3370dd3ca37a
Essam Al Joubori <essam.aljoubori@gmail.com>
2013-09-10 11:07:17 -0400

    Created a development database and product table.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'c0bb363b11d708b2f41d2a3276d64906f7905f940a618289b41b3519a6dbdcbba68611fdd5ee952e36424c4197536197ba17ca82246ed4f3f119af34b7757e6d'
